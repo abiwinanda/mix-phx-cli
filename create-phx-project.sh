83753ddc5fbf89413ac4fc089ffc269d79158672
abiwinanda <abiwinanda@outlook.com>
2022-08-09 13:31:04 +0800

    add create phx project commands

---
+++ create-phx-project.sh
@@ -0,0 +1,16 @@
+# Create a default phoenix project.
+mix phx.new ${PROJECT_NAME}
+
+# Create an umbrella project.
+mix phx.new ${PROJECT_NAME} --umbrella
+
+# Create an ecto project within an umbrella project.
+# Ecto project usually contains the business logic of the app and it shouldn't care about about the view.
+cd ${PROJECT_NAME}/apps
+mix phx.new.ecto ${CHILD_PROJECT_NAME}
+
+# Create a web project within an umbrella project.
+# Web project usually contains the "web" layer of the app. It what allows the app to speak web.
+# Component contains in web projects are controllers, templates, view, etc.
+cd ${PROJECT_NAME}/apps
+mix phx.new.web ${CHILD_PROJECT_NAME}
---
+++ help.sh
@@ -0,0 +1,5 @@
+# Get all available mix commands
+mix help
+
+# Get help for a specific command. Yes it does not use --help flag
+mix help ${MIX_COMMAND}
